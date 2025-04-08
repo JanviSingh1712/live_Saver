ffdc2bc350cb83a28cc67662e1edae059f3b3fb0
Janvi Singh <141667236+JanviSingh1712@users.noreply.github.com>
2025-04-08 21:27:52 +0530

    Update Signup.js

--- frontend/src/screens/Signup.js
+++ frontend/src/screens/Signup.js
@@ -16,7 +16,7 @@ export default function Signup() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch("http://localhost:5000/api/createuser", {
+            const response = await fetch("https://live-saver-backend.onrender.com", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
